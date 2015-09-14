b60306c59687cadd25390523d484220c5b146253
Esption <esption@gmail.com>
2015-09-13 19:19:11 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+A script to build various cargo commands.
+
+As of the first commit, all these came from https://github.com/kud1ing/awesome-rust
+
+Running `./script.sh` will get you
+* [cargo-check](https://github.com/rsolomo/cargo-check)
+* [cargo-count](https://github.com/kbknapp/cargo-count)
+* [cargo-do](https://github.com/pwoolcoc/cargo-do)
+* [cargo-dot](https://github.com/maxsnew/cargo-dot)
+* [cargo-edit](https://github.com/killercup/cargo-edit)
+* [cargo-outdated](https://github.com/kbknapp/cargo-outdated)
+* [cargo-watch](https://github.com/passcod/cargo-watch)
+
+Running `./script.sh -y` will compile the above *along with* ones that need a nightly compiler, which is currently just
+* [cargo-script](https://github.com/DanielKeep/cargo-script)
+
---
+++ script.sh
@@ -0,0 +1,39 @@
+BASE_PATH=$PWD
+BIN_PATH="$PWD/bin"
+
+flag_nightly=false;
+
+if [ "$1" == "-y" ]; then
+	echo "SETTING TO TRUE"
+	flag_nightly=true;
+fi
+
+update() {
+	if [ ! -d "$BASE_PATH/$2" ]; then
+		git clone --recursive $1
+	fi
+	cd "$BASE_PATH/$2"
+	git pull --recurse-submodules
+	cargo update
+	cargo build --release
+	cp "$BASE_PATH/$2/target/release/$2" "$BIN_PATH"
+	cd "$BASE_PATH"
+}
+
+if [ ! -d "$BIN_PATH" ]; then
+	mkdir $BIN_PATH
+fi
+
+update "https://github.com/rsolomo/cargo-check.git" "cargo-check"
+update "https://github.com/kbknapp/cargo-count.git" "cargo-count"
+update "https://github.com/pwoolcoc/cargo-do.git" "cargo-do"
+update "https://github.com/maxsnew/cargo-dot.git" "cargo-dot"
+update "https://github.com/killercup/cargo-edit.git" "cargo-edit"
+update "https://github.com/kbknapp/cargo-outdated.git" "cargo-outdated"
+update "https://github.com/passcod/cargo-watch.git" "cargo-watch"
+
+# Those in this if statement *need* a nightly build
+if [ flag_nightly ]; then
+	update "https://github.com/DanielKeep/cargo-script.git" "cargo-script"
+fi
+
