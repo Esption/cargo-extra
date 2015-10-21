b24d2b493985db2347dda38323bea7106ef5684a
Esption <esption@gmail.com>
2015-10-21 16:05:16 -0500

    Added cargo-fmt and added stuff for the extra cargo-edit commands

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-A script to build various cargo commands.  You will also need to add the `./bin/` path into your $PATH in order to use these.  If you don't know how to do that, then how to do it is add ```PATH="$PATH:/path/to/clone/bin/"``` to ~/.bashrc
+A script to build various cargo commands.  You will also need to add the `./bin/` path into your $PATH in order to use these.  If you don't know how to do that, then how to do it is add ```PATH="$PATH:/path/to/clone/bin/"``` to ~/.bashrc.  Alternatively, you could drop them into ```/usr/local/bin/``` instead if you would prefer it be a global install rather than just for a specific user.
 
 As of the first commit, all these came from https://github.com/kud1ing/awesome-rust
 
@@ -8,6 +8,7 @@ Running `./script.sh` will get you
 * [cargo-do](https://github.com/pwoolcoc/cargo-do)
 * [cargo-dot](https://github.com/maxsnew/cargo-dot)
 * [cargo-edit](https://github.com/killercup/cargo-edit)
+* [cargo-fmt](https://github.com/pwoolcoc/cargo-fmt)
 * [cargo-outdated](https://github.com/kbknapp/cargo-outdated)
 * [cargo-watch](https://github.com/passcod/cargo-watch)
 
--- script.sh
+++ script.sh
@@ -8,6 +8,7 @@ if [ "$1" == "-y" ]; then
 fi
 
 update() {
+	echo -e "\nBuilding $2\n"
 	if [ ! -d "$BASE_PATH/$2" ]; then
 		git clone --recursive $1
 	fi
@@ -28,11 +29,17 @@ update "https://github.com/kbknapp/cargo-count.git" "cargo-count"
 update "https://github.com/pwoolcoc/cargo-do.git" "cargo-do"
 update "https://github.com/maxsnew/cargo-dot.git" "cargo-dot"
 update "https://github.com/killercup/cargo-edit.git" "cargo-edit"
+#TODO: Change update() to take an array, pass these as an array.
+	cp "$BASE_PATH/cargo-edit/target/release/cargo-add" "$BIN_PATH"
+	cp "$BASE_PATH/cargo-edit/target/release/cargo-list" "$BIN_PATH"
+	cp "$BASE_PATH/cargo-edit/target/release/cargo-rm" "$BIN_PATH"
 update "https://github.com/kbknapp/cargo-outdated.git" "cargo-outdated"
 update "https://github.com/passcod/cargo-watch.git" "cargo-watch"
+update "https://github.com/pwoolcoc/cargo-fmt.git" "cargo-fmt"
 
 # Those in this if statement *need* a nightly build
-if [ flag_nightly ]; then
+if [ $flag_nightly == true ]; then
+	echo -e "\nBuilding nightly specific now.\n"
 	update "https://github.com/DanielKeep/cargo-script.git" "cargo-script"
 fi
 
